b629757ceaf17916c0ec356779bf55284e91485c
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-13 11:30:20 +0700

    Initial commit

---
+++ lib/App/pmhiderename.pm
@@ -0,0 +1,16 @@
+package App::pmhiderename;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+#ABSTRACT: Hide modules by renaming them
+
+=head1 SYNOPSIS
+
+See L<pmhiderename> CLI.
+
+
+=head1 DESCRIPTION
